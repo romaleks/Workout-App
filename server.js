6016c143505eb5348f372d610d23cfee5856c7d8
IDDna <romanov1371@gmail.com>
2023-03-04 20:09:48 +0300

    Implement getting user profile with protection

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from 'express'
 import morgan from 'morgan'
 
 import authRoutes from './app/auth/auth.routes.js'
+import userRoutes from './app/user/user.routes.js'
 import { errorHandler, notFound } from './app/middleware/error.middleware.js'
 import { prisma } from './app/prisma.js'
 
@@ -12,31 +13,32 @@ dotenv.config()
 const app = express()
 
 async function main() {
-	if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
+  if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
-	app.use(express.json())
-	app.use('/api/auth', authRoutes)
+  app.use(express.json())
+  app.use('/api/auth', authRoutes)
+  app.use('/api/users', userRoutes)
 
-	app.use(notFound)
-	app.use(errorHandler)
+  app.use(notFound)
+  app.use(errorHandler)
 
-	const PORT = 5000
+  const PORT = 5000
 
-	app.listen(
-		PORT,
-		console.log(
-			`🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue
-				.bold
-		)
-	)
+  app.listen(
+    PORT,
+    console.log(
+      `🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue
+        .bold
+    )
+  )
 }
 
 main()
-	.then(async () => {
-		await prisma.$disconnect()
-	})
-	.catch(async e => {
-		console.log(e)
-		await prisma.$disconnect()
-		process.exit(1)
-	})
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async e => {
+    console.log(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
