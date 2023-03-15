2ce30cf35be9da470ba8630b7c716fb2fba28999
IDDna <romanov1371@gmail.com>
2023-03-15 20:37:30 +0300

    Implement showing single workout

---
+++ wa-client/src/components/screens/workouts/detail/Workout.jsx
@@ -0,0 +1,53 @@
+import { useQuery } from '@tanstack/react-query'
+import { Fragment } from 'react'
+import { useParams } from 'react-router-dom'
+
+import Loader from '../../../ui/Loader'
+
+import WorkoutLogService from '../../../../services/workout/workout-log.service'
+
+import ExerciseItem from './ExerciseItem'
+import HeaderWorkout from './HeaderWorkout'
+import styles from './Workout.module.scss'
+
+const Workout = () => {
+  const { id } = useParams()
+
+  const {
+    data: workoutLog,
+    isSuccess,
+    isLoading,
+  } = useQuery(['get workout', id], () => WorkoutLogService.getById(id), {
+    select: ({ data }) => data,
+  })
+
+  return (
+    <>
+      <HeaderWorkout isSuccess={isSuccess} workoutLog={workoutLog} />
+      <div
+        className='wrapper-inner-page'
+        style={{ paddingLeft: 0, paddingRight: 0 }}
+      >
+        <div style={{ width: '90%', margin: '0 auto' }}>
+          {/* {errorCompleted && <Alert type='error' text={errorCompleted} />} */}
+        </div>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <div className={styles.wrapper}>
+            {workoutLog?.exerciseLogs?.map((exerciseLog, index) => (
+              <Fragment key={exerciseLog.id}>
+                <ExerciseItem exerciseLog={exerciseLog} />
+                {index % 2 !== 0 && index === data.exerciseLogs.length - 1 && (
+                  <div className={styles.line} />
+                )}
+              </Fragment>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default Workout
--- wa-client/src/routes/routes.data.js
+++ wa-client/src/routes/routes.data.js
@@ -1,10 +1,11 @@
 import Auth from '../components/screens/auth/Auth'
+import ExerciseLog from '../components/screens/exercise-log/ExerciseLog'
 import Home from '../components/screens/home/Home'
 import NewExercise from '../components/screens/new-exercise/NewExercise'
 import NewWorkout from '../components/screens/new-workout/NewWorkout'
 import Profile from '../components/screens/profile/Profile'
-import ListWorkouts from '../components/screens/workout/ListWorkouts'
-import Workout from '../components/screens/workout/Workout'
+import Workout from '../components/screens/workouts/detail/Workout'
+import ListWorkouts from '../components/screens/workouts/list/ListWorkouts'
 
 export const routes = [
   {
@@ -42,12 +43,9 @@ export const routes = [
     component: ListWorkouts,
     isAuth: true,
   },
-  /*
-	{
-		path: '/exercise/:id',
-
-		component: SingleExercise,
-		isAuth: true,
-	}, */
-  ,
+  {
+    path: '/exercise/:id',
+    component: ExerciseLog,
+    isAuth: true,
+  },
 ]
